4f37cce9290ec23efdddef4604ecc2b59cefda73
Gabriel Araujo <gabrielimaaraujo@gmail.com>
2024-05-23 14:35:27 -0300

    Integrando a estrtutura base do projeto

---
+++ src/main/java/me/finalprojectsantanderjava2023/FinalProjectSantanderJava2023Application.java
@@ -0,0 +1,13 @@
+package me.finalprojectsantanderjava2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectSantanderJava2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectSantanderJava2023Application.class, args);
+	}
+
+}
